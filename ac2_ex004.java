e8f744824361a1004b3a479feab222c3025a5e1a
Andre Henrique <andremeliunas@hotmail.com>
2022-05-16 22:22:31 -0300

    Update ac2_ex004.java

--- ac2_ex004.java
+++ ac2_ex004.java
@@ -1,26 +1,31 @@
 import java.util.Scanner;
 public class ac2_ex004{
     public static void main( String[] args){
-        int sexo, idade;
-        float altura, medGeral, medIdadeHomem, medAltulher, contMih, contMam;
+        int sexo = 0, idade = 0;
+        float altura, medGeral = 0, medIdadeHomem = 0, medAltMulher = 0;
 
         Scanner s = new Scanner( System.in);
+
         for(int i=0; i<10; i++){
+            
             System.out.print("Sexo ( 0-Fem / 1-Masc /2-Outro): ");
-            sexo = s.nextInt(i);
+            sexo = s.nextInt();
 
             System.out.print("Idade: ");
-            idade = s.nextInt(i);
+            idade = s.nextInt();
 
             System.out.print("Altura: ");
             altura = s.nextFloat();
 
             if(sexo == 1){
-                medIdadeHomem = medIdadeHomem + idade(i);
-                contMih++;
+                //int somaIdade = idade + idade;
+                //medIdadeHomem = somaIdade / 10;
+                medIdadeHomem += idade;
             }
-
+        
         }
+            System.out.print(medIdadeHomem/10);
+        
 
         s.close();
     }
