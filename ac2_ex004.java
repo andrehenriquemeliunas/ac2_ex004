674c97a36941af6b3015353e2ed5fc20b295e4e6
Andre Henrique <andremeliunas@hotmail.com>
2022-05-22 16:38:31 -0300

    Update ac2_ex004.java

--- ac2_ex004.java
+++ ac2_ex004.java
@@ -6,7 +6,7 @@ public class ac2_ex004{
 
         Scanner s = new Scanner( System.in);
 
-        for(int i=0; i<3; i++){
+        for(int i=0; i<10; i++){
             
             System.out.print("Sexo (0-Fem / 1-Masc /2-Outro): ");
             sexo = s.nextInt();
@@ -17,24 +17,33 @@ public class ac2_ex004{
             System.out.print("Altura: ");
             altura = s.nextFloat();
 
-            if(sexo == 1){
-                medIdadeHomem += idade;
-            }else if(sexo == 0){
+            if(sexo == 0){
                 medAltMulher += altura;
-            }else if(idade >= 35){
-                contPer += 1;
-            }else if(sexo == 2){
+            }else if(sexo == 1){
+                medIdadeHomem += idade;
+            }else{
                 contOut++;
+            }
+
+            if(idade > 18 && idade <= 35){
+                contPer = contPer + 1;
             }
         
         }
-        //System.out.print(medIdadeHomem/5); FEITO
-        //System.out.print(medAltMulher/3);FEITO
-        //System.out.print(idade/3);
-        //System.out.print(contOut);FEITO
-        System.out.print(contPer);
+        //System.out.print(idade/1);
+        System.out.println("Média das alturas das mulheres: " + medAltMulher/10);
+        System.out.println("Média das idade dos homens: " + medIdadeHomem/10);
+        System.out.println("Pessoas que se identificaram com outros: " + contOut);
+        System.out.print("Pessoas entre 18 e 35 anos: " + contPer);
         
 
         s.close();
     }
 }
+
+
+//a) média da idade do grupo;
+//b) média da altura das mulheres; FEITO
+//c) média da idade dos homens; FEITO
+//d) quantidade de pessoas que identificaram como outros;FEITO
+//e) percentual de pessoas com idade entre 18 e 35 anos (inclusive).FEITO
